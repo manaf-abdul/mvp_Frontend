ba905a9d8c6b9052b47c32eac511d42dee752cb5
manafabdul <abdumanafp1996@gmail.com>
2023-03-25 16:59:37 +0530

    LOGIN && SIGNUP

--- src/Pages/Auth/SignUp.js
+++ src/Pages/Auth/SignUp.js
@@ -11,31 +11,54 @@ import PasswordStrengthBar from 'react-password-strength-bar';
 const SignUp = () => {
   const navigate = useNavigate()
 
+  const [otp, setOtp] = useState('');
   const [values, setValues] = useState({
     name: "",
     email: "",
-    password: ""
+    password: "",
+    confirmPassword: ""
   })
   const [show, setShow] = useState(false)
+  const [otpPage, setOtpPage] = useState(false)
 
   const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value })
   }
 
-  const submitHandler = async (e) => {
+  const signUpHandler = async (e) => {
     e.preventDefault()
     try {
-      if (!values.name || !values.email || values.password.length<8) {
+      if (!values.name || !values.email || values.password.length < 8) {
         if (values.password.length < 8) {
           toast.error("Passwprd should be atleast 8 characters")
         } else {
           toast.info("Please fill all fields")
         }
       } else {
-        const { data } = await axios.post(`${BASEURL}/api/user/signup`, values)
-        toast.success("SignUp Successfull")
-        navigate('/login')
+        const payload = {
+          email: values.email,
+          name: values.name,
+          password: values.password,
+          confirmPassword: values.confirmPassword
+        }
+        const { data } = await axios.post(`${BASEURL}/auth/register`, payload)
+        setOtpPage(true)
       }
+    } catch (error) {
+      toast.error(error.response.data)
+    }
+  }
+
+  const submitHandler = async () => {
+    try {
+      const payload = {
+        email: values.email,
+        otp: Number(otp)
+      }
+      const {data} = await axios.post(`${BASEURL}/auth/verify-otp`, payload)
+      console.log("data", data)
+      toast.success("SignUp Successfull")
+      navigate('/login')
     } catch (error) {
       toast.error(error.response.data)
     }
@@ -45,65 +68,103 @@ const SignUp = () => {
     <div>
       <div className='container'>
         <div className='form-box'>
-          <h1>SignUp</h1>
-          <button className='google-btn'><img src="./google.png"></img>Signup with Google</button>
-          <hr />
-          <form onSubmit={submitHandler}>
-            <div className='form-input'>
-              <label>Name</label>
-              <input type="text"
-                placeholder="Enter your Name"
-                name="name"
-                value={values.name}
-                onChange={handleChange}
-              ></input>
-            </div>
-            <div className='form-input'>
-              <label>Email</label>
-              <input
-                type="email"
-                name="email"
-                placeholder="Enter your Email"
-                value={values.email}
-                required
-                onChange={handleChange}
-              >
-              </input>
-            </div>
-            <div className='form-input'>
-              <label>Password</label>
-              <div className='password-input'>
-                <input
-                  type={show ? "text" : "password"}
-                  placeholder="Enter password"
-                  name="password"
-                  value={values.password}
-                  onChange={handleChange}
-                >
-                </input>
-                <span
-                  onClick={() => setShow(!show)}>
-                  {show ?
-                    <FontAwesomeIcon icon={faEyeSlash} />
-                    :
-                    <FontAwesomeIcon icon={faEye} />}
-                </span>
-              </div>
-              <PasswordStrengthBar password={values.password} />
-            </div>
-            <div className='form-check-input' style={{ marginTop: "6px" }}>
-              <input id="check-box" type="checkbox" placeholder="name"></input>
-              <label htmlFor='check-box'>i agree with <Link>terms</Link> And <Link>Privacy</Link></label>
-            </div>
-            <div className='form-input'>
-              <button type="submit">SignUp</button>
-            </div>
-            <hr />
-            <div className='footer-text'>
-              <p>Already have an account?</p>
-              <Link to={'/'}>Login</Link>
-            </div>
-          </form>
+          <h1 style={{ textAlign: "center" }}>Sign Up</h1>
+          {
+            otpPage ?
+              <>
+                <hr />
+                <div className='form-input'>
+                  <label style={{ marginLeft: "7rem" }}>Enter your Otp</label>
+                  <input
+                    type={"password"}
+                    placeholder="Enter OTP"
+                    name="confirmPassword"
+                    value={otp}
+                    onChange={(e) => setOtp(e.target.value)}
+                  >
+                  </input>
+
+                </div>
+                <div className='form-input'>
+                  <button onClick={submitHandler}>Verify OTP</button>
+                </div>
+              </>
+              :
+              <>
+                <button className='google-btn'><img src="./google.png"></img>Sign up with Google</button>
+                <hr />
+                <form onSubmit={signUpHandler}>
+                  <div className='form-input'>
+                    <label>Name</label>
+                    <input type="text"
+                      placeholder="Enter your Name"
+                      name="name"
+                      value={values.name}
+                      onChange={handleChange}
+                    ></input>
+                  </div>
+                  <div className='form-input'>
+                    <label>Email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Enter your Email"
+                      value={values.email}
+                      required
+                      onChange={handleChange}
+                    >
+                    </input>
+                  </div>
+                  <div className='form-input'>
+                    <label>Password</label>
+                    <div className='password-input'>
+                      <input
+                        type={show ? "text" : "password"}
+                        placeholder="Enter password"
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                      >
+                      </input>
+                      <span
+                        onClick={() => setShow(!show)}>
+                        {show ?
+                          <FontAwesomeIcon icon={faEyeSlash} />
+                          :
+                          <FontAwesomeIcon icon={faEye} />}
+                      </span>
+                    </div>
+                  </div>
+
+                  <PasswordStrengthBar password={values.password} />
+                  <div className='form-input' style={{ marginTop: "3px" }}>
+                    <label>Confirm Password</label>
+                    <div className='password-input'>
+                      <input
+                        type={"password"}
+                        placeholder="Enter password"
+                        name="confirmPassword"
+                        value={values.confirmPassword}
+                        onChange={handleChange}
+                      >
+                      </input>
+                    </div>
+                  </div>
+                  <div className='form-check-input' style={{ marginTop: "6px" }}>
+                    <input id="check-box" type="checkbox" placeholder="name"></input>
+                    <label htmlFor='check-box'>i agree with <Link>terms</Link> And <Link>Privacy</Link></label>
+                  </div>
+                  <div className='form-input'>
+                    <button type="submit">Sign Up</button>
+                  </div>
+                  <hr />
+                  <div className='footer-text'>
+                    <p>Already have an account?</p>
+                    <Link to={'/'}>Login</Link>
+                  </div>
+                </form>
+              </>
+          }
         </div>
       </div>
     </div>
